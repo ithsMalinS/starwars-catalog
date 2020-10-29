c95ae5428aae94bd80345b32202d16220ae5561f
Simon <soold88@hotmail.se>
2020-10-29 15:16:17 +0100

    tjo
    tjo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+async function renderDetails(charL) {
+  let t = await fetchData(charL.homeworld);
+  const details = document.querySelector(".character-details");
+  const planets = document.querySelector(".planet-details");
+  await console.log(t);
+  details.innerHTML =
+  `<h4>${charL.name}</h4><p>${charL.height}</p><p>${charL.mass}</p><p>${charL.hair_color}</p><p>${charL.skin_color}</p><p>${charL.eye_color}</p><p>${charL.birth_year}</p><p>${charL.gender}</p>`;
+  planets.innerHTML = `<h4>${t.name}</h4><p>${t.rotation_period}</p><p>${t.orbital_period}</p><p>${t.diameter}</p><p>${t.climate}</p><p>${t.gravity}</p><p>${t.terrain}</p>`
+}
 async function renderPeople(charL) {
   const characterL = document.querySelector(".character-list > ul");
   characterL.innerHTML =
@@ -5,10 +14,13 @@ async function renderPeople(charL) {
   const characters = document.querySelectorAll(".character-list > ul > li");
 
   for (let i = 0; i < characters.length; i++) {
-    console.log(charL[i]);
     characters[i].innerText = charL[i].name;
+    characters[i].addEventListener("click", function () {
+      renderDetails(charL[i]);
+    });
   }
 }
+
 async function fetchData(url) {
   //document.querySelector("").classList.remove("hidden");
   let request = await fetch(url);
