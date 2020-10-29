cf428e9015e93571a5e97cb45536fa8d2f828ba2
Malin Söderström <malin.soderstrom@iths.se>
2020-10-29 16:11:14 +0100

    hoho

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
+let counter = 0
+
 async function renderDetails(charL) {
   let t = await fetchData(charL.homeworld);
   const details = document.querySelector(".character-details");
   const planets = document.querySelector(".planet-details");
   await console.log(t);
   details.innerHTML =
-  `<h4>${charL.name}</h4><p>${charL.height}</p><p>${charL.mass}</p><p>${charL.hair_color}</p><p>${charL.skin_color}</p><p>${charL.eye_color}</p><p>${charL.birth_year}</p><p>${charL.gender}</p>`;
-  planets.innerHTML = `<h4>${t.name}</h4><p>${t.rotation_period}</p><p>${t.orbital_period}</p><p>${t.diameter}</p><p>${t.climate}</p><p>${t.gravity}</p><p>${t.terrain}</p>`
+  `<h4>${charL.name}</h4><p>Height: ${charL.height} cm</p><p>Mass: ${charL.mass} kg</p><p>Hair color: ${charL.hair_color}</p><p>Skin color: ${charL.skin_color}</p><p>Eye color: ${charL.eye_color}</p><p>Birth year: ${charL.birth_year}</p><p>Gender: ${charL.gender}</p>`;
+  planets.innerHTML = `<h4>${t.name}</h4><p>Rotation period: ${t.rotation_period} hours</p><p>Orbital period: ${t.orbital_period} days</p><p>Diameter: ${t.diameter} km</p><p>Climate: ${t.climate}</p><p>Gravity: ${t.gravity}</p><p>Terrain: ${t.terrain}</p>`
 }
 async function renderPeople(charL) {
   const characterL = document.querySelector(".character-list > ul");
@@ -16,7 +18,12 @@ async function renderPeople(charL) {
   for (let i = 0; i < characters.length; i++) {
     characters[i].innerText = charL[i].name;
     characters[i].addEventListener("click", function () {
+      for(character of characters) {
+        character.classList.remove('chosen-character')
+      }
       renderDetails(charL[i]);
+      characters[i].classList.add("chosen-character")
+      //characters[i].innerText += '▸'
     });
   }
 }
